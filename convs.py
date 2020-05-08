b716a0012701efe831ae8e2ece2f6674e377c114
Galina <igneous.galaxy003@gmail.com>
2020-05-08 21:33:27 +1000

    .

--- convs.py
+++ convs.py
@@ -1,17 +1,17 @@
-f = open('movie_conversations.txt')
+# до 32 строки идет обработка текстов с диалогами из фильмов
+f = open('texts/movie_conversations.txt')
 conversations = f.readlines()
 f.close()
 # словарь, в котором ключ - это первая фраза, а значение - следующая за ней (фразы в виде меток)
 line_after_line = dict()
 # сценарии диалогов с метками
 for i in range(len(conversations)):
-    conversations[i] = eval(conversations[i].split(' +++$+++ ')[-1][:-1])
+    conversations[i] = eval(
+        conversations[i].split(' +++$+++ ')[-1][:-1])  # сценарий диалога это список и вот его можно достать из файла
     for j in range(len(conversations[i]) - 1):
-        line_after_line[conversations[i][j]] = conversations[i][j + 1]
-
-main_lines = line_after_line.keys()
-
-f = open('movie_lines.txt')
+        line_after_line[conversations[i][j]] = conversations[i][j + 1]  # добавляем данные в словарь
+main_lines = line_after_line.keys()  # id реплик, на которые можно ответить
+f = open('texts/movie_lines.txt')
 movie_lines = f.readlines()
 f.close()
 # словарь с метками фраз и самими фразами и наборот
@@ -25,7 +25,26 @@ inv_lines = {value: key for key, value in lines.items()}
 # выводит диалог №1238
 # for line in conversations[1238]:
 #     print(lines[line])
-text = ''
+
+movie_text = ''
 for i in main_lines:
-    text += lines[i]
-text = text.replace('\n', ' ')
+    movie_text += lines[i]
+
+f = open('texts/Architecture of London.txt')
+arch_lines = f.readlines()
+f.close()
+f = open('texts/slime molds.txt')
+slime_lines = f.readlines()
+f.close()
+
+
+def text(some_lines):
+    txt = ''
+    for line in some_lines:
+        if line:
+            txt += ' ' + line
+    return txt
+
+
+london_text = text(arch_lines)
+slime_text = text(slime_lines)
--- dialogue.py
+++ dialogue.py
@@ -8,10 +8,12 @@ import convs
 # nltk.download('punkt', quiet=True)
 # nltk.download('wordnet', quiet=True)
 
-RANDOM_RESPONSES = ['ExTeRmiNaTe', 'I love cheese!', 'I don\'t understand. And what? Gonna bully me?',
+RANDOM_RESPONSES = ['ExTeRmiNaTe', 'I don\'t understand. And what? Gonna bully me?',
                     'It\'s time to stop!', 'WHAT?', 'You know...', 'Ask google', 'I\'m not interested']
 
-sent_tokens = nltk.sent_tokenize(convs.text)
+movie_lines_sent_tokens = nltk.sent_tokenize(convs.movie_text)
+london_sent_tokens = nltk.sent_tokenize(convs.london_text)
+slime_sent_tokens = nltk.sent_tokenize(convs.slime_text)
 remove_punctuation_dict = dict((ord(pun), None) for pun in string.punctuation)
 
 
@@ -19,14 +21,14 @@ def LemNormalize(text):
     return nltk.word_tokenize(text.lower().translate(remove_punctuation_dict))
 
 
-def response(user_response):
+def weird_response(user_response):
     user_response = user_response.lower()
     bot_response = ''
-    sent_tokens.append(user_response)  # добавим ответ пользователя в список предложений
+    movie_lines_sent_tokens.append(user_response)  # добавим ответ пользователя в список предложений
 
     # create TfidfVectorizer Object
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
-    tfidf = TfidfVec.fit_transform(sent_tokens)
+    tfidf = TfidfVec.fit_transform(movie_lines_sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
     idx = vals.argsort()[0][-2]  # индекс самого похожего на ответ пользователя предложения
     flat = vals.flatten()
@@ -36,14 +38,53 @@ def response(user_response):
         bot_response += random.choice(RANDOM_RESPONSES)
     else:
         try:
-            metka = convs.inv_lines[sent_tokens[idx] + '\n']
+            metka = convs.inv_lines[movie_lines_sent_tokens[idx] + '\n']
             next_m = convs.line_after_line[metka]
             resp = convs.lines[next_m]
             bot_response += resp
         except Exception:
             try:
-                bot_response += sent_tokens[idx]
+                bot_response += movie_lines_sent_tokens[idx]
             except Exception:
                 bot_response += random.choice(RANDOM_RESPONSES)
-    sent_tokens.remove(user_response)
+    movie_lines_sent_tokens.remove(user_response)  # удалим сообщение пользователя из списка предложений
+
     return bot_response
+
+
+def normal_response(user_response, sent_tokens):
+    user_response = user_response.lower()
+    bot_response = ''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    score = flat[-2]
+    if score == 0:
+        bot_response += random.choice(RANDOM_RESPONSES)
+    else:
+        bot_response += sent_tokens[idx]
+    sent_tokens.remove(user_response)
+
+    return bot_response
+
+
+def which_response(user_response, number):
+    if number == 1:
+        return normal_response(user_response, london_sent_tokens)
+    elif number == 0:
+        return weird_response(user_response)
+    elif number == 2:
+        return normal_response(user_response, slime_sent_tokens)
+
+# testing
+# while True:
+#     print('!!')
+#     a = input()
+#     if a == 0:
+#         break
+#     else:
+#         print(which_response(a, int(input())))
